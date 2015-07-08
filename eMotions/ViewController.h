06f43bc3557d62d41d624d83acd42f416f0506f7
Cory Alder <coryalder@gmail.com>
2015-07-08 08:04:01 -0400

    First Commit

---
+++ eMotions/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  eMotions
+//
+//  Created by Cory Alder on 2015-07-08.
+//  Copyright (c) 2015 Cory Alder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@import CoreMotion;
+
+@interface ViewController : UIViewController
+
+
+@end
+
